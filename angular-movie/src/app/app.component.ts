45e51dc1748a72408fea0d0d31448f751085b26c
Tshepiso28 <tshepisostudent@gmail.com>
2025-03-14 08:51:47 +0200

    Creating routes

--- angular-movie/src/app/app.component.ts
+++ angular-movie/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, MovieListComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, MovieListComponent, NavbarComponent, FooterComponent, AboutUsComponent],
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
--- angular-movie/src/app/app.routes.ts
+++ angular-movie/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: AppComponent},
+    { path: 'about', component: AboutUsComponent },
+];
